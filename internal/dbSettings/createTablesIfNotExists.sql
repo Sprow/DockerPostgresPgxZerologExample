a4ec951a7626bf32bb00c60141b95ad4edd1a521
Sprow <ts.sprow@gmail.com>
2022-06-01 13:35:57 +0300

    db indexes example

--- internal/dbSettings/createTablesIfNotExists.sql
+++ internal/dbSettings/createTablesIfNotExists.sql
@@ -7,3 +7,8 @@ CREATE TABLE IF NOT EXISTS important_data(
     data2 VARCHAR(50),
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
+-- create indexes example
+create index if not exists important_data_data1_index on important_data (data1);
+create index if not exists important_data_data2_index on important_data (data2);
+create index if not exists important_data_created_at_index on important_data (created_at);
